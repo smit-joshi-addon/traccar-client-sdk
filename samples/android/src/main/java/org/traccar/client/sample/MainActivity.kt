151013985032b498349c9b37f87ddc1bb031504d
Anton Tananaev <anton.tananaev@gmail.com>
2026-05-25 07:27:21 -0700

    Fix status bar color

--- samples/android/src/main/java/org/traccar/client/sample/MainActivity.kt
+++ samples/android/src/main/java/org/traccar/client/sample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -30,6 +31,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             MaterialTheme {
                 TrackerScreen()
